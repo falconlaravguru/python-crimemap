3ff14d304da1da7ead843b1dfd74c260ded54a64
falconlaravguru <falcon.laravguru@yandex.com>
2017-10-27 22:35:10 +1300

    Basic Code Structure

---
+++ dbhelper.py
@@ -0,0 +1,38 @@
+import pymysql
+import dbconfig
+
+class DBHelper:
+    def connect(self, database="crimemap"):
+        return pymysql.connect(host="localhost",
+                               user=dbconfig.db_user,
+                               password=dbconfig.db_password,
+                               db=database)
+    def get_all_data(self):
+        connection = self.connect()
+        try:
+            query = "SELECT description from crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            return cursor.fetchall()
+        finally:
+            connection.close()
+    
+    def add_input(self, data):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (description) VALUE ('{}');".format(data)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            connection.commit()
+        finally:
+            connection.close()
+
+    def clear_all(self):
+        connection = self.connect()
+        try:
+            query = "DELETE FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            connection.commit()
+        finally:
+            connection.close()
