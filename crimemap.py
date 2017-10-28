62e6976698eb5b96cc3beffcdcf8ef9855035ea9
falconlaravguru <falcon.laravguru@yandex.com>
2017-10-28 22:36:39 +1300

    Add Validation User Input

--- crimemap.py
+++ crimemap.py
@@ -3,6 +3,11 @@ from flask import request
 from flask import render_template
 import json
 
+import datetime
+import dateparser
+
+import string
+
 import dbconfig
 if dbconfig.test:
     from mockdbhelper import MockDbHelper as DBHelper
@@ -12,15 +17,18 @@ else:
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging','break-in']
+
 @app.route('/')
-def home():
+def home(error_message=None):
+    crime_data = {}
     try:
         data=DB.get_all_data()
-        crimes = json.dumps(data)
+        crime_data = json.dumps(data)
     except Exception as e:
         print e
         data=None
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crime_data, categories=categories, error_message=error_message)
 
 @app.route('/add', methods=["POST"])
 def add():
@@ -43,12 +51,27 @@ def clear():
 @app.route('/submitcrime', methods=["POST"])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
     latitude = request.form.get("latitude")
     longitude = request.form.get("longitude")
-    description = request.form.get("description")
+    description = sanitize_string(request.form.get("description"))
     DB.add_input(category,date,latitude,longitude,description)
     return home()
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
